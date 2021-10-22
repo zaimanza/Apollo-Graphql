fe253a1759b0ac727bb09c327475107aa8dd20bb
MANZA <36519974+zaimanza@users.noreply.github.com>
2021-10-23 01:19:59 +0800

    j

--- middleware/pubsubs.js
+++ middleware/pubsubs.js
@@ -2,8 +2,52 @@ var {
     PubSub,
     withFilter,
 } = require('graphql-subscriptions');
+var {
+    RedisPubSub,
+} = require('graphql-redis-subscriptions');
+var Redis = require('ioredis');
+var {
+    spy,
+    restore,
+    stub,
+} = require('simple-mock');
 
-const pubsub = new PubSub(); //create a PubSub instance
+const options = {
+    // host: 'localhost',
+    // port: '9092',
+    retryStrategy: times => {
+        // reconnect after
+        return Math.min(times * 50, 2000);
+    }
+};
+
+const publishSpy = spy((channel, message) => listener && listener(channel, message));
+const subscribeSpy = spy((channel, cb) => cb && cb(null, channel));
+const unsubscribeSpy = spy((channel, cb) => cb && cb(channel));
+const psubscribeSpy = spy((channel, cb) => cb && cb(null, channel));
+const punsubscribeSpy = spy((channel, cb) => cb && cb(channel));
+const quitSpy = spy(cb => cb);
+
+const mockRedisClient = {
+    publish: publishSpy,
+    subscribe: subscribeSpy,
+    unsubscribe: unsubscribeSpy,
+    psubscribe: psubscribeSpy,
+    punsubscribe: punsubscribeSpy,
+    on: (event, cb) => {
+        if (event === 'message') {
+            listener = cb;
+        }
+    },
+    quit: quitSpy,
+};
+
+const mockOptions = {
+    publisher: (mockRedisClient),
+    subscriber: (mockRedisClient),
+};
+
+const pubsub = new RedisPubSub(mockOptions);
 
 exports.pubsub = pubsub;
 exports.withFilter = withFilter;
